e6a413d37ee1e07ab7bea59ee666787db490be57
CTeam30 <33256528+CTeam30@users.noreply.github.com>
2019-02-04 14:17:39 +0000

    Update Sketch.js with left arrow clear

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+let value = 0
 function setup() {
   createCanvas(800, 450);
 	background(120);
 }
 
 function draw() {
-	fill(255, 255, 255, 20, 70)
+	fill(value, value, value, 20, 70)
 	rect(mouseX, mouseY, 20, 20)
+}
+
+function keyPressed() {
+		if (keyCode === LEFT_ARROW) {
+			clear()
+		}
+	return false
 }
