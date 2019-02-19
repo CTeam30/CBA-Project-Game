15d69e7646d31ddf8f751af29e475b3d171e8f8d
CTeam30 <33256528+CTeam30@users.noreply.github.com>
2019-02-19 21:16:29 +0000

    Update sketch.js with vertical and horizontal movment

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let horizontal = 400
+let vertical = 200
 
 function setup() {
 	createCanvas(800, 450);
@@ -8,7 +9,7 @@ function setup() {
 function draw() {
 	noStroke()
 	fill(255, 255, 255, 20, 70)
-	rect(horizontal, 200, 20, 20)
+	rect(horizontal, vertical, 20, 20)
 }
 
 function keyPressed() {
@@ -17,8 +18,21 @@ function keyPressed() {
 		clear()
 		background(50);
 	}
+	
 	if (keyCode === RIGHT_ARROW) {
 		horizontal += 10
+		clear()
+		background(50);
+	}
+	
+	if (keyCode === UP_ARROW) {
+		vertical -= 10
+		clear()
+		background(50);
+	}
+	
+	if (keyCode === DOWN_ARROW) {
+		vertical += 10
 		clear()
 		background(50);
 	}
