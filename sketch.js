67e2ae26b0ee3e19b5486a8af9befccb64d86126
CTeam30 <33256528+CTeam30@users.noreply.github.com>
2019-02-19 22:00:30 +0000

    Addition of barriers and ground

--- sketch.js
+++ sketch.js
@@ -1,21 +1,41 @@
-let horizontal = 400
+let horizontal = 0
 let vertical = 200
+let bx = 50
+let by = 200
+let start = 1
 
 function setup() {
 	createCanvas(800, 450);
 	background(50);
+	stroke(255, 0, 0)
+	line(0, 220, 500, 220)
 }
 
 function draw() {
-		if (vertical < 200) {
+/*	if (start = 1) {
+		setInterval(100)
+	  horizontal += 1
+		clear()
+		background(50) 
+
+	}*/
+	if (vertical < 200) {
 		setInterval(10)
-		vertical +=3
+		vertical += 3
 		clear()
 		background(50)
 	}
 	noStroke()
 	fill(255, 255, 255, 20, 70)
 	rect(horizontal, vertical, 20, 20)
+	//Drawing of main block
+	noStroke()
+	fill(155, 0, 0, 20, 70)
+	rect(bx, by, 20, 20)
+	//Drawing of barrier
+	stroke(255, 0, 0)
+	line(0, 220, 500, 220)
+	//Drawing of line on ground
 
 }
 
@@ -26,6 +46,26 @@ function keyPressed() {
 		clear()
 		background(50);
 	}
+
+	if (keyCode === RIGHT_ARROW) {
+		horizontal += 10
+		clear()
+		background(50);
+	}
+
+	if (keyCode === UP_ARROW) {
+		vertical -= 50
+		clear()
+		background(50);
+	}
+
+	if (keyCode === DOWN_ARROW) {
+		vertical += 10
+		clear()
+		background(50);
+	}
+	return false
+}
 	
 	if (keyCode === RIGHT_ARROW) {
 		horizontal += 10
