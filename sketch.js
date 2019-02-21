e4b684bb4aabac43e871a6b6320fd1674e0aaebe
CTeam30 <33256528+CTeam30@users.noreply.github.com>
2019-02-21 19:10:35 +0000

    Most functionality added

--- sketch.js
+++ sketch.js
@@ -1,3 +1,12 @@
+var game = {
+	life : 3,
+	dead : 0,
+	level : 1,
+	wait : 0,
+	gravitypull : 1,
+	jump : 0,
+}
+
 var block = {
 	x : 0,
 	y : 200,
@@ -13,14 +22,30 @@ var platform = {
 	y : 200,
 }
 
+var platform = {
+	x : 0,
+	y : 200,
+}
+
+var reward = {
+	x : 0,
+	y : 200,
+}
+
 let stamina = 0
 let extraCanvas;
 
 function setup() {
 	createCanvas(600, 400);
 	platform.x = random(90, 480)
-	platform.y = random(200, 0)
+	platform.y = random(110, 180)
 
+	barrier.x = random(platform.x +30, platform.x - 90)
+	barrier.y = random(platform.y +30, platform.y - 90)
+	
+	reward.x = random(platform.x +30, platform.x - 90)
+	reward.y = random(platform.y -30, platform.y - 90)
+	
 	extraCanvas = createGraphics(600, 400);
 	extraCanvas.clear()
 	background(50);
@@ -29,13 +54,49 @@ function setup() {
 }
 
 function draw() {
-
+	
 	let d = dist(block.x, block.y, barrier.x, barrier.y)
-	if (d < 20) {
-		background(0, 255, 0)
+	if (d < 20 && game.wait < 1) {
+		game.wait += 1000
+		game.life = game.life - 1
 	}
-
-	if (block.y < 200) {
+	//barrier and block distance detection
+	
+	let e = block.y - platform.y
+	let f = block.x - platform.x
+	if (e > -21 && e < -20 && f < 20 && f > -20) {
+		game.gravitypull = 0
+		game.jump = 1
+	} else {
+		game.gravitypull = 1
+	}
+	//platform and block distance detection
+	
+	let b = dist(barrier.x, barrier.y, platform.x, platform.y)
+	if (b > 100 || b < 30) {
+		barrier.x = random(platform.x +30, platform.x - 90)
+		barrier.y = random(platform.y +30, platform.y - 90)
+		clear()		
+	}
+	//barrier and platform distance detection
+	
+	let r = dist(reward.x, reward.y, platform.x, platform.y)
+	if (r > 100 || r < 30) {
+		reward.x = random(platform.x +30, platform.x - 90)
+		reward.y = random(platform.y -30, platform.y - 90)
+		clear()
+	}
+	//reward and platform distance detection
+	
+	let t = dist(barrier.x, barrier.y, reward.x, reward.y)
+	if (t > 100 || t < 30) {
+		reward.x = random(platform.x +30, platform.x - 90)
+		reward.y = random(platform.y -30, platform.y - 90)
+		clear()
+	}
+	//reward and barrier distance detection
+	
+	if (block.y < 200 && game.gravitypull === 1) {
 		setInterval(10);
 		block.y += 2;
 		clear();
@@ -55,9 +116,14 @@ function draw() {
 	//Drawing of barrier
 
 	extraCanvas.noStroke();
-	extraCanvas.fill(155, 155, 0, 20, 70);
+	extraCanvas.fill(0, 155, 155, 20, 70);
 	extraCanvas.rect(platform.x, platform.y, 20, 20);
 	//Drawing of platform
+	
+	extraCanvas.noStroke();
+	extraCanvas.fill(155, 155, 0, 20, 70);
+	extraCanvas.rect(reward.x, reward.y, 20, 20);
+	//Drawing of reward
 
 	noStroke();
 	fill(255, 255, 255, 20, 70);
@@ -70,9 +136,14 @@ function draw() {
 
 	image(extraCanvas, 0, 0);
 	
-	if (block.y >= 200) {
+	if (block.y >= 200 || game.jump === 1) {
 			stamina = 0
 			//print(stamina)
+	
+	if (game.wait > 1) {
+		setInterval(100)
+		game.wait = 0
+	}
 	}
 		
 	
@@ -82,13 +153,13 @@ function draw() {
 
 function keyPressed() {
 	if (keyCode === LEFT_ARROW) {
-		block.x -= 10;
+		block.x -= 20;
 		clear()
 		//background(50);
 	}
 
 	if (keyCode === RIGHT_ARROW) {
-		block.x += 10;
+		block.x += 20;
 		clear();
 		//background(50);
 	}
@@ -96,7 +167,7 @@ function keyPressed() {
 		if (keyCode === UP_ARROW) {
 			block.y -= 70;
 			stamina = stamina + 1
-			print(stamina)
+		//	print(stamina)
 			clear();
 			//background(50);
 		}
