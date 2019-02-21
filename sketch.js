7b07c7e930521f532ef3a708fb1621fc55f87b34
CTeam30 <33256528+CTeam30@users.noreply.github.com>
2019-02-21 17:10:34 +0000

    Large Update

--- sketch.js
+++ sketch.js
@@ -1,88 +1,111 @@
-let horizontal = 0
-let vertical = 200
-let bx = 50
-let by = 200
-let start = 1
+var block = {
+	x : 0,
+	y : 200,
+}
+
+var barrier = {
+	x : 50,
+	y : 200,
+}
+
+var platform = {
+	x : 0,
+	y : 200,
+}
+
+let stamina = 0
+let extraCanvas;
 
 function setup() {
-	createCanvas(800, 450);
+	createCanvas(600, 400);
+	platform.x = random(90, 480)
+	platform.y = random(200, 0)
+
+	extraCanvas = createGraphics(600, 400);
+	extraCanvas.clear()
 	background(50);
-	stroke(255, 0, 0)
-	line(0, 220, 500, 220)
+	stroke(255, 0, 0);
+	line(0, 220, 500, 220);
 }
 
 function draw() {
-/*	if (start = 1) {
-		setInterval(100)
-	  horizontal += 1
-		clear()
-		background(50) 
 
-	}*/
-	if (vertical < 200) {
-		setInterval(10)
-		vertical += 3
-		clear()
-		background(50)
+	let d = dist(block.x, block.y, barrier.x, barrier.y)
+	if (d < 20) {
+		background(0, 255, 0)
 	}
-	noStroke()
-	fill(255, 255, 255, 20, 70)
-	rect(horizontal, vertical, 20, 20)
-	//Drawing of main block
-	noStroke()
-	fill(155, 0, 0, 20, 70)
-	rect(bx, by, 20, 20)
+
+	if (block.y < 200) {
+		setInterval(10);
+		block.y += 2;
+		clear();
+		//background(50);
+	}
+
+	if (block.y > 200) {
+		setInterval(10);
+		block.y -= 1;
+		clear();
+		//background(50);
+	}
+
+	extraCanvas.noStroke();
+	extraCanvas.fill(155, 0, 0, 20, 70);
+	extraCanvas.rect(barrier.x, barrier.y, 20, 20);
 	//Drawing of barrier
-	stroke(255, 0, 0)
-	line(0, 220, 500, 220)
+
+	extraCanvas.noStroke();
+	extraCanvas.fill(155, 155, 0, 20, 70);
+	extraCanvas.rect(platform.x, platform.y, 20, 20);
+	//Drawing of platform
+
+	noStroke();
+	fill(255, 255, 255, 20, 70);
+	rect(block.x, block.y, 20, 20);
+	//Drawing of main block
+
+	extraCanvas.stroke(255, 0, 0);
+	extraCanvas.line(0, 220, 500, 220);
 	//Drawing of line on ground
 
+	image(extraCanvas, 0, 0);
+	
+	if (block.y >= 200) {
+			stamina = 0
+			//print(stamina)
+	}
+		
+	
 }
+
 
 
 function keyPressed() {
 	if (keyCode === LEFT_ARROW) {
-		horizontal -= 10
+		block.x -= 10;
 		clear()
-		background(50);
+		//background(50);
 	}
 
 	if (keyCode === RIGHT_ARROW) {
-		horizontal += 10
-		clear()
-		background(50);
+		block.x += 10;
+		clear();
+		//background(50);
 	}
-
-	if (keyCode === UP_ARROW) {
-		vertical -= 50
-		clear()
-		background(50);
+	if (stamina < 2) {
+		if (keyCode === UP_ARROW) {
+			block.y -= 70;
+			stamina = stamina + 1
+			print(stamina)
+			clear();
+			//background(50);
+		}
 	}
 
 	if (keyCode === DOWN_ARROW) {
-		vertical += 10
-		clear()
-		background(50);
+		block.y += 10;
+		clear();
+		//background(50);
 	}
-	return false
-}
-	
-	if (keyCode === RIGHT_ARROW) {
-		horizontal += 10
-		clear()
-		background(50);
-	}
-	
-	if (keyCode === UP_ARROW) {
-		vertical -= 50
-		clear()
-		background(50);
-	}
-	
-	if (keyCode === DOWN_ARROW) {
-		vertical += 10
-		clear()
-		background(50);
-	}
-	return false
+	return false;
 }
